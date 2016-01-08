9cb1f29ad3c836d55a1022d7a3fff53222eafc3a
choconnie <hj.c.cho@gmail.com>
2016-01-08 23:52:25 +0000

    testing branch for OOP

--- two_player_game.rb
+++ two_player_game.rb
@@ -1,32 +1,35 @@
 require 'colorize'
 
-@p1_lives = 3
-@p2_lives = 3
-@p1_score = 0
-@p2_score = 0
-num = Random.new
-@num1 = 0
-@num2 = 0
-player = 'Player 1'
-run_game = true
+class Player
+  attr_accessor :name, :lives, :score
+
+  def initialize(name)
+    @name = name
+    @lives = 3
+    @score = 0
+  end
+
+  def get_score?(result)
+    puts result
+    result ? @score += 5 : @score -= 5
+  end
+
+  def lose_lives?(result)
+    result ? @lives : @lives -= 1
+  end
+
+end
+
 
 def correct_answer?(answer)
   correct = @num1 + @num2
   correct == answer
 end
 
-def get_score?(result, player)
-  result ? player += 5 : player -= 5
-end
-
-def lose_lives?(result, player)
-  result ? player : player -= 1
-end
-
-def win?
-  if @p1_score > @p2_score
+def win?(p1_score, p2_score)
+  if p1_score > p2_score
     return "Player 1 win."
-  elsif @p1_score < @p2_score
+  elsif p1_score < p2_score
     return "Player 2 win."
   else
     return "Player 1 and Player 2 are even."
@@ -34,37 +37,47 @@ def win?
 end
 
 
-puts "Game Start!"
+
+person = []
+p1 = Player.new('Player 1')
+p2 = Player.new('Player 2')
+person[0] = [p1.name, p1.lives, p1.score ,p1]
+person[1] = [p2.name, p2.lives, p2.score ,p2]
+
+num = Random.new
+@num1 = 0
+@num2 = 0
+whos_turn = 0
+run_game = true
+
+puts "-------------"
+puts " Game Start!".red
+puts "-------------"
 
 while run_game
-  
   @num1 = num.rand(1..20)
   @num2 = num.rand(1..20)
 
-  puts "#{player}: What does #{@num1} plus #{@num2} equal?"
+  p = person[whos_turn]
+  puts "#{p[0]}: What does #{@num1} plus #{@num2} equal?"
   answer = gets.chomp.to_i
+  
   result = correct_answer?(answer)
+  p[2] = p[3].get_score?(result)
+  p[1] = p[3].lose_lives?(result)
+  puts "#{p[0]}: #{p[1]} lives left.".yellow
+  puts "P1 score: #{person[0][2]}".blue
+  puts "P2 score: #{person[1][2]}".green
 
-  if player == 'Player 1'
-    @p1_score = get_score?(result, @p1_score)
-    @p1_lives = lose_lives?(result, @p1_lives)
-  else
-    @p2_score = get_score?(result, @p2_score)
-    @p2_lives = lose_lives?(result, @p2_lives)
-  end
-  puts "P1 score: #{@p1_score}".blue
-  puts "P2 score: #{@p2_score}".green
-
-  if @p1_lives == 0 || @p2_lives == 0
-    who = win?
+  if p[1] == 0
+    who = win?(p1.score, p2.score)
     puts "-------------------------------"
     puts "Congrats!! #{who}".red
-    puts "Player 1 score: #{@p1_score}".blue
-    puts "Player 2 score: #{@p2_score}".green
+    puts "Player 1 score: #{p1.score}".blue
+    puts "Player 2 score: #{p2.score}".green
     puts "-------------------------------"
     run_game = false
   end
 
-  player == 'Player 1' ? player = 'Player 2' : player = 'Player 1'
-
+  whos_turn == 0 ? whos_turn = 1 : whos_turn = 0
 end
